d79331219e4196f88a24f5830cfd8ea564c4f920
iuk <iuk@iukonline.com>
2013-08-07 23:25:04 +0200

    Episode table scraping refactored

--- TVShowScraperWikipedia.php
+++ TVShowScraperWikipedia.php
@@ -97,73 +97,130 @@ class TVShowScraperWikipedia extends TVShowScraper {
 			$res = Array();
 
 			$tables = Array();
-			if (preg_match_all('/\{\|[^\}]+\|\}/', $content, $tables)) {
+			#if (preg_match_all('/\{\|[^\}]+\|\}/', $content, $tables)) {
+			if (preg_match_all('/\{\|.*?\|\}/s', $content, $tables)) {
 				$this->log("Found " . sizeof($tables[0]) . " tables");
 				foreach ($tables[0] as $t) {
-					//$this->log("Checking table $t");
-					//if (preg_match('/!\s*n.?\s*!!?\s*titolo/i', $t)) {
-					if (preg_match('/!\s*n\S*\s*!+\s*titolo/i', $t)) {
-						$this->log("Found TV Show table");
-						$rows = Array();
-						if (preg_match_all('/\|\-.*?\|(.*?)[\n\|]\|(.*?)[\n\|]\|(.*?)[\n\|]\|(.*?)[\n\|]\|(.*?)(?=\|[\-\}])/s', $t, $rows, PREG_SET_ORDER)) {
-							$this->log("Found " . sizeof($rows) . " episodes.");
-							foreach ($rows as $episode) {
-								for ($i = 1; $i < 6; $i++) {
-									$episode[$i] = preg_replace('/<ref[^>]*>.*<\/ref>/i', '', $episode[$i]);
-								}
-								$ep = trim($episode[1]);
-								$strDate = trim(preg_replace('/[^a-z0-9 ]+/i' , '', $episode[5]));
-								
-								
-								
-								$this->log("Episode: $ep, prima TV Italia: $strDate");
+				
+					$headerStart = strpos($t, "\n!");
+					if ($headerStart === FALSE) {
+						$this->log("Table has no header. Skipping...");
+						continue;
+					}
 
-								$retVal = !$showOnlyNew;
-							
-								$episodeDB = $this->tvdb->getEpisodeFromIndex($seasonData['tvshow'], $seasonData['n'], $ep);
-								if ($episodeDB === FALSE) {
-									if ($saveResults) {
-										$this->log("Creating episode $ep");
-										$episodeDB = $this->tvdb->getEpisode($this->tvdb->addEpisode($seasonData['id'], Array('n' => $ep)));
-										$retVal = TRUE;
-									}
-									if ($showOnlyNew) {
-										$retVal = TRUE;
-									}
-								}
-								if ($episodeDB && strlen($strDate > 0)) {
-									$dateItems = Array();
-									if (preg_match('/(\d+)\s+([A-Za-z]+)\s?(\d\d\d\d)\b/', $strDate, $dateItems)) {
-										switch(substr(strtolower($dateItems[2]), 0, 3)) {
-											case 'gen': $dateItems[2] = 1; break;
-											case 'feb': $dateItems[2] = 2; break;
-											case 'mar': $dateItems[2] = 3; break;
-											case 'apr': $dateItems[2] = 4; break;
-											case 'mag': $dateItems[2] = 5; break;
-											case 'giu': $dateItems[2] = 6; break;
-											case 'lug': $dateItems[2] = 7; break;
-											case 'ago': $dateItems[2] = 8; break;
-											case 'set': $dateItems[2] = 9; break;
-											case 'ott': $dateItems[2] = 10; break;
-											case 'nov': $dateItems[2] = 11; break;
-											case 'dic': $dateItems[2] = 12; break;
-										}
-										$date = strtotime("$dateItems[3]-$dateItems[2]-$dateItems[1]");
-										
-										$this->log("airDate ($dateItems[3]-$dateItems[2]-$dateItems[1]) in seconds: $date");
-											
-										if (! isset($episodeDB['airDate']) || $episodeDB['airDate'] != $date) {
-											if ($saveResults) {
-												$this->tvdb->setEpisode($episodeDB['id'], Array('airDate' => $date));
-												$retVal = TRUE;
-											}
-										}
-									}	
-								}
-								if ($retVal) {
-									$res[] = Array('n' => $ep, 'airDate' => $date);
-								}
+					$headerStop = strpos($t, "\n|-", $headerStart);
+					if ($headerStop === FALSE) {
+						$this->log("Table has no rows. Skipping...");
+						continue;
+					}
+
+					$header = substr($t, $headerStart + 2, $headerStop - $headerStart - 2);
+					$this->log("Table header $header");
+
+					$hcols = array();
+					$nIndex = FALSE;
+					$airIndex = FALSE;
+
+					$hlines = explode("\n!", $header);
+					foreach ($hlines as $h) {
+						$subh = explode("!!", $h);
+						foreach ($subh as $hh) {
+							$pipeIndex = strpos($hh, '|');
+							if ($pipeIndex != FALSE) $hh = substr($hh, $pipeIndex + 1);
+
+							$this->log("Column " . sizeof($hcols) . " title: $hh");
+							$hcols[] = $hh;
+							if (preg_match('/n[º°]/i', $hh)) {
+								$nIndex = sizeof($hcols) - 1;
+								$this->log("n index = $nIndex");
+							} else if (preg_match('/prima\s+tv\s+italia/i', $hh)) {
+								$airIndex = sizeof($hcols) - 1;
+								$this->log("n index = $airIndex");
 							}
+						}
+					}
+
+					if ($nIndex === FALSE || $airIndex === FALSE) {
+						$this->log("Not a TV Show table. Skipping...");
+						continue;
+					}
+
+					$body = substr($t, $headerStop + 3, strlen($t) - $headerStop - 5);
+					$rows = explode("\n|-", $body);
+					foreach ($rows as $r) {
+						$i = 0;
+						$ep = FALSE;
+						$strDate = FALSE;
+
+						$r = trim($r, " \t\n\r\0\x0B|");
+						$rcols = explode("\n|", $r);
+						foreach ($rcols as $subcol) {
+							$col = explode("||", $subcol);
+							foreach ($col as $c) {
+								if ($i == $nIndex) {
+									$ep = $c;
+								} else if ($i == $airIndex) {
+									$strDate = $c;
+								}
+								$i++;
+							}
+						}
+
+						if ($ep === FALSE || $strDate === FALSE) {
+							$this->log("Some column missing, skipping row...");
+							continue;
+						}
+
+						$ep = trim(preg_replace('/<ref[^>]*>.*<\/ref>/i', '', $ep));
+						$strDate = trim(preg_replace('/<ref[^>]*>.*<\/ref>/i', '', $strDate));
+						$strDate = (preg_replace('/[^a-z0-9 ]+/i', '', $strDate));
+
+						$this->log("Episode: $ep, prima TV Italia: $strDate");
+
+						$retVal = !$showOnlyNew;
+							
+						$episodeDB = $this->tvdb->getEpisodeFromIndex($seasonData['tvshow'], $seasonData['n'], $ep);
+						if ($episodeDB === FALSE) {
+							if ($saveResults) {
+								$this->log("Creating episode $ep");
+								$episodeDB = $this->tvdb->getEpisode($this->tvdb->addEpisode($seasonData['id'], Array('n' => $ep)));
+								$retVal = TRUE;
+							}
+							if ($showOnlyNew) {
+								$retVal = TRUE;
+							}
+						}
+						if ($episodeDB && strlen($strDate > 0)) {
+							$dateItems = Array();
+							if (preg_match('/(\d+)\s+([A-Za-z]+)\s?(\d\d\d\d)\b/', $strDate, $dateItems)) {
+								switch(substr(strtolower($dateItems[2]), 0, 3)) {
+									case 'gen': $dateItems[2] = 1; break;
+									case 'feb': $dateItems[2] = 2; break;
+									case 'mar': $dateItems[2] = 3; break;
+									case 'apr': $dateItems[2] = 4; break;
+									case 'mag': $dateItems[2] = 5; break;
+									case 'giu': $dateItems[2] = 6; break;
+									case 'lug': $dateItems[2] = 7; break;
+									case 'ago': $dateItems[2] = 8; break;
+									case 'set': $dateItems[2] = 9; break;
+									case 'ott': $dateItems[2] = 10; break;
+									case 'nov': $dateItems[2] = 11; break;
+									case 'dic': $dateItems[2] = 12; break;
+								}
+								$date = strtotime("$dateItems[3]-$dateItems[2]-$dateItems[1]");
+								
+								$this->log("airDate ($dateItems[3]-$dateItems[2]-$dateItems[1]) in seconds: $date");
+									
+								if (! isset($episodeDB['airDate']) || $episodeDB['airDate'] != $date) {
+									if ($saveResults) {
+										$this->tvdb->setEpisode($episodeDB['id'], Array('airDate' => $date));
+										$retVal = TRUE;
+									}
+								}
+							}	
+						}
+						if ($retVal) {
+							$res[] = Array('n' => $ep, 'airDate' => $date);
 						}
 					}
 				}

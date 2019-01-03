632a265444bda1c6d24b8a29a55355b36eb96440
iuk <iuk@iukonline.com>
2019-01-03 10:37:47 +0100

    Now working

--- catLogs.sh
+++ catLogs.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-if [ -e "$1"/api.*.log ]; then
-	for l in $(ls -rt "$1"/api.*.log); do
-		echo Archiving "$l"...
-		cat "$l" >> "$1/api.log"
-		rm -f "$l"
-	done
-fi
+for l in $(ls -rt "$1"/api.*.log 2>/dev/null); do
+	echo Archiving "$l"...
+	cat "$l" >> "$1/api.log"
+	rm -f "$l"
+done
